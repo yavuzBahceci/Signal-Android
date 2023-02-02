d41f701bdc6f98ae7589dc56ec9b4f198dc5a3f4
yavuzBahceci <myavuzbahceci@gmail.com>
2023-02-02 17:33:39 +0300

    format delegation added for photopage list.

--- domain/src/main/java/com/app/signal/domain/model/PhotoListPage.kt
+++ domain/src/main/java/com/app/signal/domain/model/PhotoListPage.kt
@@ -1,12 +1,31 @@
 package com.app.signal.domain.model
 
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
 data class PhotoListPage<out T>(
     val items: List<T>,
-    val nextPage: Long?
+    val pageCount: Long,
+    val currentPage: Long
 ) {
+    val nextPage: Long? by FormatDelegate()
+
     companion object {
         fun <T> empty(): PhotoListPage<T> {
-            return PhotoListPage(emptyList(), null)
+            return PhotoListPage(emptyList(), 0, 0)
         }
     }
+}
+
+class FormatDelegate<T> :
+    ReadOnlyProperty<PhotoListPage<T>, Long?> {
+
+    override fun getValue(thisRef: PhotoListPage<T>, property: KProperty<*>): Long? {
+        return if (thisRef.currentPage < thisRef.pageCount) {
+            thisRef.currentPage + 1
+        } else {
+            null
+        }
+    }
+
 }
